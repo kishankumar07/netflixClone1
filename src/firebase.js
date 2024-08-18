9a08b9808cc840279df0d6d3944466d7840a7509
Kishan T A <kishantashok@gmail.com>
2024-08-18 20:11:06 +0530

    Firebase auth done | next is animation at login page | react-toastify done

---
+++ src/firebase.js
@@ -0,0 +1,66 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+import { createUserWithEmailAndPassword,
+         getAuth,
+         signInWithEmailAndPassword,
+         signOut } from "firebase/auth";
+
+import { addDoc,
+         collection,
+         getFirestore } from "firebase/firestore";
+import { toast } from "react-toastify";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app)
+
+const signup = async(name,email,password)=>{
+    try{
+
+            const res = await createUserWithEmailAndPassword(auth,email,password);
+            console.log('res is :',res);
+            const user = res.user;
+            await addDoc(collection(db,'user'),{
+                uid:user.uid,
+                name,
+                authProvider:'local',
+                email,
+            })
+            console.log('user added to firestore successfully')
+    }catch(error){
+        console.error('error at signup :',error)
+        toast.error(error.code.split('/')[1].split('-').join(' '));
+        // toast.error(error.code)
+    }
+} 
+
+
+const login =async(email,password) =>{
+    try{
+        await signInWithEmailAndPassword(auth,email,password);
+    }catch(error){
+        console.error('error at login: ',error);
+        toast.error(error.code.split('/')[1].split('-').join(' '));
+    }
+}
+
+
+const logout = ()=>{
+    signOut(auth);
+}
+
+export{auth,db,signup,login,logout}
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,22 +1,39 @@
 import './Login.css'
 import logo from '../../assets/logo.png'
 import { useState } from 'react'
+import { signup,login } from '../../firebase'
 
 const Login = () => {
 
   const [signState,setSignstate] = useState('Sign In')
+  const [name,setName] = useState('');
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+
+  const user_auth = async(event)=>{
+    event.preventDefault();
+    if(signState === 'Sign In'){
+      await login(email,password)
+    }else{
+      await signup(name,email,password);
+    }
+  }
 
   return (
     <div className="login">
         <img src={logo} className='login-logo' alt="" />
         <div className="login-form">
+
           <h1>{signState}</h1>
+
           <form action="">
-            {signState === 'Sign Up' ? <input type="text" placeholder='Your name' /> : <></>}
+            {signState === 'Sign Up' ? <input value={name} onChange={(e)=>{setName(e.target.value)}} type="text" placeholder='Your name' /> : <></>}
             
-            <input type="email" placeholder='Email'/>
-            <input type="password" placeholder='Password'/>
-            <button>{signState}</button>
+            <input value={email} onChange={(e)=>{setEmail(e.target.value)}} type="email" placeholder='Email'/>
+            <input value={password} onChange={(e)=>{setPassword(e.target.value)}} type="password" placeholder='Password'/>
+
+            <button onClick={()=>{user_auth(event)}} type='submit'>{signState}</button>
+
             <div className="form-help">
                 <div className="remember">
                   <input type="checkbox" />
@@ -24,11 +41,12 @@ const Login = () => {
                 </div>
                 <p>Need Help?</p>
             </div>
+
           </form>
+
+
           <div className="form-switch">
             {signState === 'Sign In' ? <p>New to Netflix? <span onClick={()=>{setSignstate('Sign Up')}}>Sign up now</span></p> :  <p>Already have account? <span onClick={()=>{setSignstate('Sign Inll')}}>Sign in now</span></p>}
-            
-           
           </div>
         </div>
     </div>
